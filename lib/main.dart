1e9700909dc74b2dcb6f66e402d7f855b4c5b9d4
yeonjoo.lee <teddyjune@naver.com>
2022-09-27 16:55:18 +0900

    feat: companyListing Screen 작성

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:provider/provider.dart';
+import 'package:stock_info_app/data/repository/stock_repository_impl.dart';
 import 'package:stock_info_app/data/source/local/company_listing_entity.dart';
+import 'package:stock_info_app/data/source/local/stock_dao.dart';
+import 'package:stock_info_app/data/source/remote/stock_api.dart';
+import 'package:stock_info_app/presentation/company_listings/company_listingd_veiw_model.dart';
+import 'package:stock_info_app/presentation/company_listings/company_listings_screen.dart';
 import 'package:stock_info_app/util/color_schemes.dart';
 
 void main() async {
   await Hive.initFlutter();
   Hive.registerAdapter(CompanyListingEntityAdapter());
-  runApp(const MyApp());
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(
+        create: (_) => CompanyListingsViewModel(
+          StockRepositoryImpl(
+            StockApi(),
+            StockDao(),
+          ),
+        ),
+      )
+    ],
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -25,70 +43,7 @@ class MyApp extends StatelessWidget {
         colorScheme: darkColorScheme,
       ),
       themeMode: ThemeMode.system,
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-      bottomNavigationBar: NavigationBar(
-        destinations: const [
-          NavigationDestination(
-            icon: Icon(Icons.explore),
-            label: 'Explore',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.commute),
-            label: 'Commute',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.bookmark),
-            label: 'Bookmark',
-          ),
-        ],
-      ),
+      home: const CompanyListingsScreen(),
     );
   }
 }
